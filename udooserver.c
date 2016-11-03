e2f73f6e1eb0fd51a2eca546dd7ef3997e503e1d
Francesco Montefoschi <francesco.monte@gmail.com>
2016-11-03 12:05:30 +0100

    Version 1.3
    - Android support: firmware is stored on /data/m4last.fw
    - Removed compiler warnings
    - Removed memset on buffer
    - Synced filesystem upon firmware update
    - Improved source style consistency

--- udooserver.c
+++ udooserver.c
@@ -21,27 +21,36 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+
 #ifdef ANDROID
 #include <sys/wait.h>
-#endif
+#define LOG_TAG "FotaServer"
+#include <cutils/log.h>
+#define LogDebug ALOGD
+#define LogError ALOGE
+#define M4_FIRMWARE_RECEIVED_FILE       "/data/m4last.fw"
+#define M4_UPLOADER                     "mqx_upload_on_m4SoloX"
+#else //ANDROID
+#define LogDebug printf
+#define LogError perror
+#define M4_FIRMWARE_RECEIVED_FILE       "M4_file.fw"
+#define M4_UPLOADER                     "udooneo-m4uploader"
+#endif //ANDROID
 
-#define UDOONEO_SERVER_PORT	    5152
-#define BUF_SIZE                256
-#define NMAX_REMOTE_CMD         4
-#define MAX_SIZE_REMOTE_CMD			32
-#define VERSION						"1.2"		// added GET_UPLOADER_STATUS
-#define ACK	                    "*"
-#define NACK                    "?"
-#define NAME_OF_BOARD	          "UDOONeo"
-#define M4_FIRMWARE_RECEIVED_FILE	"M4_file.fw"
-#define M4_UPLOADER             "udooneo-m4uploader"
-
+#define UDOONEO_SERVER_PORT             5152
+#define BUF_SIZE                        256
+#define NMAX_REMOTE_CMD                 4
+#define MAX_SIZE_REMOTE_CMD             32
+#define VERSION                         "1.3"
+#define ACK                             "*"
+#define NACK                            "?"
+#define NAME_OF_BOARD	                "UDOONeo"
 
 enum {
 	TEST = 1,
 	FW_UPLOADER,
 	GET_UPLOADER_STATUS
-}eRemoteCommandCode;
+} eRemoteCommandCode;
 
 const char *tabRemoteCmd [NMAX_REMOTE_CMD] = {
 	"NO_CMD",
@@ -51,9 +60,9 @@ const char *tabRemoteCmd [NMAX_REMOTE_CMD] = {
 };
 
 int statusUploader = 0;
-const unsigned char strEOF[] = "********????????@@@@@@@@++++++++";
+const char strEOF[] = "********????????@@@@@@@@++++++++";
 
-unsigned char ReceivedStrEOF(unsigned char *buff)
+unsigned char ReceivedStrEOF(char *buff)
 {
 	int i;
 	unsigned char isEOF=1;
@@ -61,57 +70,61 @@ unsigned char ReceivedStrEOF(unsigned char *buff)
 	for (i=0; i<strlen(strEOF); i++) {
 		if (buff[i] != strEOF[i]) isEOF=0;
 	}
-	return (isEOF);
+	return isEOF;
 }
 
+/**
+ * Reads from the connfd socket and writes to filename.
+ *
+ * @param int connfd The socket to read
+ * @param const char * filename File where the firmware will be stored
+ * @return int Bytes received. <0 on error.
+ */
 int savefile(int connfd, const char * filename)
 {
-    /* Create file where data will be stored */
-    FILE *fp = fopen(filename, "wb");
-    if(NULL == fp)
-    {
-       printf("Error opening file %s\n", filename);
-       return 1;
-    }
+	FILE *fp = fopen(filename, "wb");
+	if(NULL == fp) {
+		LogDebug("Error opening file %s\n", filename);
+		return -1;
+	}
 
-    /* Receive data in chunks of BUF_SIZE bytes */
-    int bytesReceived = 0;
-    int totBytesReceived = 0;
-    char buff[BUF_SIZE];
-    memset(buff, '0', sizeof(buff));
-    while((bytesReceived = read(connfd, buff, BUF_SIZE)) > 0)
-    {
-	    if (bytesReceived == strlen(strEOF)) {
-		    if (ReceivedStrEOF(buff)) {
-			    printf ("received end of file\n");
-			    break;
-		    }
-	    }
-	    fwrite(buff, 1, bytesReceived, fp);
-	    if (bytesReceived > 0) totBytesReceived += bytesReceived;
-    }
+	/* Receive data in chunks of BUF_SIZE bytes */
+	int bytesReceived = 0;
+	int totBytesReceived = 0;
+	char buff[BUF_SIZE] = {0};
+	
+	while((bytesReceived = read(connfd, buff, BUF_SIZE)) > 0) {
+		if (bytesReceived == (int)strlen(strEOF)) {
+			if (ReceivedStrEOF(buff)) {
+				LogDebug("Received end of file\n");
+				break;
+			}
+		}
+		fwrite(buff, 1, bytesReceived, fp);
+		if (bytesReceived > 0) totBytesReceived += bytesReceived;
+	}
 
-    if(bytesReceived < 0)
-    {
-	    printf("\n Read Error \n");
-    }
+	if(bytesReceived < 0) {
+		LogError("Read Error!\n");
+		return -1;
+	}
 
-    fclose(fp);
-    return (totBytesReceived);
+	fclose(fp);
+	sync();
+	return totBytesReceived;
 }
 
 int GetRemoteCommand (int connfd)
 {
 	char tBuffer[MAX_SIZE_REMOTE_CMD];
 	char cmdBuffer[MAX_SIZE_REMOTE_CMD];
-	int msgLen = 0;
 	int bytesReceived = 0;
 	int cmd = 0;
 
 	cmdBuffer[0] = 0;
 	do {
 		bytesReceived = read(connfd, tBuffer, MAX_SIZE_REMOTE_CMD);
-		//printf ("bytesReceived = %d\r\n", bytesReceived);
+		LogDebug("bytesReceived = %d\r\n", bytesReceived);
 		if (bytesReceived > 0) strcat (cmdBuffer, tBuffer);
 		if ((strchr(tBuffer,0x0D) != NULL) || (strchr(tBuffer,0x0A) != NULL)) break;
 	}while (bytesReceived > 0);
@@ -133,11 +146,6 @@ int GetRemoteCommand (int connfd)
 	return (cmd);
 }
 
-int upload (void)
-{
-	return (1);
-}
-
 int DoRemoteCommand (int connfd, int cmd)
 {
 	const char * filename = M4_FIRMWARE_RECEIVED_FILE;
@@ -147,37 +155,37 @@ int DoRemoteCommand (int connfd, int cmd)
 
 	switch (cmd) {
 	case FW_UPLOADER:
-		printf("Uploader command received\n");
+		LogDebug("Uploader command received\n");
 		fsz=savefile(connfd, filename);
 		if (fsz > 0) {
-			printf("Received fileSize = %d\n", fsz);
+			LogDebug("Received fileSize = %d\n", fsz);
 
 			char command[256] = "" ;
 			strcat(command, uploader);
 			strcat(command," ");
 			strcat(command, filename);
 
-			printf("Command = %s\n", command);
+			LogDebug("Command = %s\n", command);
 			// cast to unsigned char because command program return unsigned char
 			statusUploader = system(command);
 			statusUploader = WEXITSTATUS(statusUploader);
-			printf ("statusUploader = %d\n", statusUploader);
+			LogDebug("statusUploader = %d\n", statusUploader);
 		}
 		else {
 			write(connfd,NACK,1);
-			printf("Received file error!!\n");
+			LogError("Received file error!\n");
 			retValue = 1;
 		}
 		break;
 
 	case GET_UPLOADER_STATUS:
-		printf("GET_M4_STATUS received\n");
+		LogDebug("GET_M4_STATUS received\n");
 		unsigned char stUpld = (unsigned char)statusUploader;
 		write(connfd,&stUpld,1);
 		break;
 
 	case TEST:
-		printf("Test command received\n");
+		LogDebug("Test command received\n");
 		break;
 
 	default:
@@ -193,15 +201,14 @@ int main(int argc, char** argv)
 	int serv_port = UDOONEO_SERVER_PORT;
 	int pp;
 
-    if (argc == 2)
-    {
+	if (argc == 2) {
 		if (strcmp(argv[1], "-h") == 0) {
-			printf ("Usage: %s [server port]\n", argv[0]);
-			exit (EXIT_SUCCESS);
+			printf("Usage: %s [server port]\n", argv[0]);
+			exit(EXIT_SUCCESS);
 		} else {
 			if (strcmp(argv[1], "-v") == 0) {
-				printf ("%s %s\n", argv[0], VERSION);
-				exit (EXIT_SUCCESS);
+				printf("%s %s\n", argv[0], VERSION);
+				exit(EXIT_SUCCESS);
 			}
 			else {
 				pp = atoi(argv[1]);
@@ -209,65 +216,65 @@ int main(int argc, char** argv)
 					serv_port = pp;
 				}
 				else {
-					printf ("bad server port number !!\n");
-					exit (EXIT_SUCCESS);
+					printf("bad server port number !!\n");
+					exit(EXIT_SUCCESS);
 				}
 			}
 		}
-    }
+	}
  
 	int listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	if (listenfd < 0) {
-		perror("Socket open");
+		LogError("Cannot open socket");
 		exit (EXIT_FAILURE);
 	}
 
-    struct sockaddr_in serv_addr;
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(serv_port);
+	struct sockaddr_in serv_addr;
+	memset(&serv_addr, '0', sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(serv_port);
 
 	int reuse = 1;
-	if ( setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(int)) == -1 )
+	if ( setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(int)) == -1 ) {
+		LogError("Cannot set socket options");
+		exit (EXIT_FAILURE);
+	}
+
+	if (bind(listenfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr)) < 0) {
+		LogError("Cannot bind socket");
+		exit (EXIT_FAILURE);
+	}
+
+	if (listen(listenfd, 10) == -1) {
+		LogError("Cannot listen on the socket");
+		exit (EXIT_FAILURE);
+	}
+	
+	LogDebug("Listening connections on port %d\n", serv_port);
+
+	for (;;)
 	{
-		perror("setsockopt");
-		exit (EXIT_FAILURE);
-	}
-
-    if (bind(listenfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr)) < 0) {
-		perror("bind");
-		exit (EXIT_FAILURE);
-	}
-
-    if (listen(listenfd, 10) == -1)
-    {
-		perror("listen");
-		exit (EXIT_FAILURE);
-    }
-
-    for (;;)
-    {
-        int connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL);
-		printf("Connection accepted on %d port\n", serv_port);
+		int connfd = accept(listenfd, (struct sockaddr*)NULL ,NULL);
+		LogDebug("Connection accepted on port %d\n", serv_port);
 
 		int rCmd;
 		do {
-			rCmd = GetRemoteCommand (connfd);
+			rCmd = GetRemoteCommand(connfd);
 			if (rCmd > 0) {
-				if (DoRemoteCommand(connfd, rCmd) == 0)
-					printf ("Remote Command [%d] done\n", rCmd);
-				else
-					printf ("Remote Command [%d] failed\n", rCmd);
+				if (DoRemoteCommand(connfd, rCmd) == 0) {
+					LogDebug("Remote Command [%d] done\n", rCmd);
+				} else {
+					LogDebug("Remote Command [%d] failed\n", rCmd);
+				}
 			}
 		} while (rCmd >= 0);
-		//int tt=savefile(connfd, filename);
-		printf("Close connection on %d port\n", serv_port);
-        close(connfd);
-        sleep(1);
-    }
 
-	exit (EXIT_FAILURE);
+		LogDebug("Closed connection on port %d\n", serv_port);
+		close(connfd);
+		sleep(1);
+	}
+
+	exit(EXIT_FAILURE);
 }
-
